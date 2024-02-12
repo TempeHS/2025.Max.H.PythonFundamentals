74af41b8934335a6b84b78301105bbc0034664c1
Maxwelltempe123 <158523723+Maxwelltempe123@users.noreply.github.com>
2024-02-12 01:39:32 +0000

    Upload completed

---
+++ 1-Conditionals/deep/deep.py
@@ -0,0 +1,10 @@
+life = input("what is the answer to life? ")
+
+if life == "42":
+    print("Yes")
+elif life == "Forty Two":
+    print("Yes")
+elif life == "forty-two":
+    print("Yes")
+else:
+    print("No")
