cff6fdd5dac9df5f40f43ceb0626ac8877af7d46
Maxwelltempe123 <158523723+Maxwelltempe123@users.noreply.github.com>
2024-02-17 08:48:13 +0000

    upload completed meal.py

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -1,20 +1,27 @@
-def convert(time: str):
-    hours, minutes = time.split(":")
-    x = float(minutes) / 60
-    y = float(hours) + x
-    return y
-
-
 def main():
     time = input("whats the time: ")
-    print(convert(time))
+    meal = check_meal_time(time)
+    if meal:
+        print("It's", meal, "time.")
+    else:
+        print("dont eat")
 
 
-if "7.0" <= y <= "8.0":
-    print("it is Breakfast")
-elif "12.0" <= y <= "13.0" or "1.00":
-    print("its lunch")
-elif ""
+def convert(time):
+    hours, minutes = time.split(":")
+    return float(hours) + float(minutes) / 60
+
+
+def check_meal_time(time):
+    hours = convert(time)
+    if 7 <= hours < 8:
+        return "breakfast"
+    elif 12 <= hours < 13:
+        return "lunch"
+    elif 18 <= hours < 19:
+        return "dinner"
+    else:
+        return None
 
 
 if __name__ == "__main__":
