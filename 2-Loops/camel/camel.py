b1af2348662d1681702a7d84af3b16ed97853bf3
Maxwelltempe123 <158523723+Maxwelltempe123@users.noreply.github.com>
2024-02-25 07:09:14 +0000

    upload completed snake.py

--- 2-Loops/camel/camel.py
+++ 2-Loops/camel/camel.py
@@ -1,2 +1,21 @@
 def camel_to_snake(camel_case):
     snake_case = ""
+    for char in camel_case:
+        if char.isupper():
+            snake_case += "_" + char.lower()
+        else:
+            snake_case += char
+
+    if snake_case.startswith("_"):
+        snake_case = snake_case[1:]
+    return snake_case
+
+
+def main():
+    camel_case = input("Enter a variable name in camel case: ")
+    snake_case = camel_to_snake(camel_case)
+    print("Snake case:", snake_case)
+
+
+if __name__ == "__main__":
+    main()
