bb98bd12970b8e2fb7ca3be40f8fcbfb4f1d2abb
Maxwelltempe123 <158523723+Maxwelltempe123@users.noreply.github.com>
2024-05-22 23:26:03 +0000

    uploading

---
+++ 8-OOP/3ddiagramclasswork/3ddiagramclasswork.py
@@ -0,0 +1,61 @@
+import math
+
+class shape3d:
+    def __init__(self, colour, x, y, z):
+        self.colour = colour
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def area_2d():
+
+    def volume():
+
+    def perimeter():
+
+    def xyzcoord():
+
+    def volume():
+
+    def surface_area():
+
+    def surface_count():
+    
+
+class cube3d:
+    def __init__(self, colour, x, y, z):
+        super().__init__(self, colour, x, y, z)
+        self.length = length
+
+    def area_2d(self):
+        return self.length ** 2
+    def volume(self):
+        return self.length ** 3
+    def perimeter(self):
+        return 4 * self.length
+    def xyzcoord(self):
+        return 
+    def surface_area(self):
+        return 6 * self.length ** 2
+    def surface_count(self):
+        return 6
+class sphere3d:
+    def __init__(self, colour, x, y, z):
+        super().__init__(self, colour, x, y, z)
+        self.radius = radius
+    def area_2d(radius):
+        return self.radius/2 * 3.14 ** 2
+    def volume(radius):
+        return 4/3 * 3.14 * radius ** 3
+    def perimeter(radius):
+        return 2 * 3.14 * radius
+    def xyzcoord(radius):
+        return 
+    def surface_area(radius):
+        return 4 * 3.14 ** 2
+    def surface_count(radius):
+        return 6
+
+
+if __name__ == "__main__":
+    main()
